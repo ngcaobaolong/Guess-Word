9df77992a112ee54f49ca60ee2d022ba3ebdf9ee
ngcaobaolong <41871863+ngcaobaolong@users.noreply.github.com>
2020-12-19 17:14:30 +0700

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,15 +1,80 @@
 CREATE DATABASE IF NOT EXISTS guess_word;
 USE guess_word;
-CREATE TABLE IF NOT EXISTS users (
-    userID int(11) NOT NULL AUTO_INCREMENT,
-    userName VARCHAR(500) NOT NULL,
-    userEmail VARCHAR(500) NOT NULL,
-    userPassword VARCHAR(500) NOT NULL,
-    userPoint int(11) NOT NULL DEFAULT 0,
-    PRIMARY KEY (userID)
-);
-CREATE TABLE IF NOT EXISTS words (
-    id INT(11) NOT NULL AUTO_INCREMENT,
-    word VARCHAR(100) NOT NULL,
-    PRIMARY KEY (id)
-);
+-- MySQL dump 10.13  Distrib 8.0.22, for Linux (x86_64)
+--
+-- Host: localhost    Database: guess_word
+-- ------------------------------------------------------
+-- Server version	8.0.22-0ubuntu0.20.04.3
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `userID` int NOT NULL AUTO_INCREMENT,
+  `userName` varchar(500) NOT NULL,
+  `userEmail` varchar(500) NOT NULL,
+  `userPassword` varchar(500) NOT NULL,
+  `userPoint` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`userID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'abc','asd@gmail.com','3c9909afec25354d551dae21590bb26e38d53f2173b8d3dc3eee4c047e7ab1c1eb8b85103e3be7ba613b31bb5c9c36214dc9f14a42fd7a2fdb84856bca5c44c2',0),(2,'123','123@gmail.com','3c9909afec25354d551dae21590bb26e38d53f2173b8d3dc3eee4c047e7ab1c1eb8b85103e3be7ba613b31bb5c9c36214dc9f14a42fd7a2fdb84856bca5c44c2',915);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `words`
+--
+
+DROP TABLE IF EXISTS `words`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `words` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `word` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=128 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `words`
+--
+
+LOCK TABLES `words` WRITE;
+/*!40000 ALTER TABLE `words` DISABLE KEYS */;
+INSERT INTO `words` VALUES (1,'chrome'),(2,'firefox'),(3,'codepen'),(4,'javascript'),(5,'jquery'),(6,'twitter'),(7,'github'),(8,'wordpress'),(9,'opera'),(10,'sass'),(11,'layout'),(12,'standards'),(13,'semantic'),(14,'designer'),(15,'developer'),(16,'module'),(17,'component'),(18,'website'),(19,'creative'),(20,'banner'),(21,'browser'),(22,'screen'),(23,'mobile'),(24,'footer'),(25,'header'),(26,'typography'),(27,'responsive'),(28,'programmer'),(29,'css'),(30,'border'),(31,'compass'),(32,'grunt'),(33,'pixel'),(34,'document'),(35,'object'),(36,'ruby'),(37,'modernizr'),(38,'bootstrap'),(39,'python'),(40,'php'),(41,'pattern'),(42,'ajax'),(43,'node'),(44,'element'),(45,'android'),(46,'application'),(47,'adobe'),(48,'apple'),(49,'google'),(50,'microsoft'),(51,'bookmark'),(52,'internet'),(53,'icon'),(54,'svg'),(55,'background'),(56,'property'),(57,'syntax'),(58,'flash'),(59,'html'),(60,'font'),(61,'blog'),(62,'network'),(63,'server'),(64,'content'),(65,'database'),(66,'socket'),(67,'function'),(68,'variable'),(69,'link'),(70,'apache'),(71,'query'),(72,'proxy'),(73,'backbone'),(74,'angular'),(75,'email'),(76,'underscore'),(77,'cloud');
+/*!40000 ALTER TABLE `words` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-12-19 17:12:18
